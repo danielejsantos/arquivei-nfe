d9112dd89c2cdf2cd01f6fc00baf87148a142bdb
danielejsantos <js_dani@hotmail.com>
2021-04-20 11:34:50 -0300

    Create global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+    height: 100%;
+    width: 100%;
+  }
+  *, button, input {
+    border: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+  :root {
+    --primary: #4076FA;
+    --success: #00D233;
+    --error: #F15025;
+    --alert: #E8C547;
+    --dark: #001634;
+    --extra-dark: #091223;
+    --grey: #a7a7a7;
+  }
+`;
