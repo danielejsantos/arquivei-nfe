6779c55663cbb69f69f5ccedc2cbc4bae2ca0ae3
danielejsantos <js_dani@hotmail.com>
2021-04-26 20:21:06 -0300

    Fix header responsiveness

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,12 +8,32 @@ export const Container = styled.div`
   position: relative;
   box-sizing: border-box;
   box-shadow: 0px 7px 5px -2px rgba(0, 0, 0, 0.1);
+
+  @media (max-width: 740px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: static;
+  }
 `;
 
 export const LogoImg = styled.img`
   left: 35px;
   width: 200px;
   position: absolute;
+
+  @media (max-width: 930px) {
+    width: 180px;
+  }
+
+  @media (max-width: 840px) {
+    width: 160px;
+  }
+
+  @media (max-width: 740px) {
+    position: static;
+    margin-bottom: 15px;
+  }
 `;
 
 export const Slogan = styled.div`
@@ -27,9 +47,25 @@ export const BoldText = styled.p`
   font-size: 32px;
   font-weight: 600;
   color: var(--dark);
+
+  @media (max-width: 930px) {
+    font-size: 28px;
+  }
+
+  @media (max-width: 840px) {
+    font-size: 24px;
+  }
 `;
 
 export const NormalText = styled.p`
   font-size: 28px;
   color: var(--extra-dark);
+
+  @media (max-width: 930px) {
+    font-size: 26px;
+  }
+
+  @media (max-width: 840px) {
+    font-size: 22px;
+  }
 `;
